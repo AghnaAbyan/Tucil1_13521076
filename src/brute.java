24d2625bcdc24e6e4537374cba848427f944a07b
AghnaAbyan <aghnamuhammad@gmail.com>
2023-01-25 00:28:47 +0700

    Kurang txt
    Kurang txt

---
+++ src/brute.java
@@ -0,0 +1,117 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.IntBinaryOperator;
+
+class card {
+    public static int convertCardToInt(String rank) {
+        switch(rank){
+            case "A":
+                return 1;
+            case "2":
+                return 2;
+            case "3":
+                return 3;
+            case "4":
+                return 4;
+            case "5":
+                return 5;
+            case "6":
+                return 6;
+            case "7":
+                return 7;
+            case "8":
+                return 8;
+            case "9":
+                return 9;
+            case "10":
+                return 10;
+            case "J":
+                return 11;
+            case "Q":
+                return 12;
+            case "K":
+                return 13;
+            default:
+                return 0;
+        }
+    }
+}
+
+class DuaPuluhEmpat {
+    private static final List<IntBinaryOperator> operators = new ArrayList<IntBinaryOperator>();
+    private static final List<Character> opnames = new ArrayList<Character>();
+
+    public DuaPuluhEmpat() {
+        operators.add((a, b) -> a + b);
+        operators.add((a, b) -> a - b);
+        operators.add((a, b) -> a * b);
+        operators.add((a, b) -> (b != 0 && a % b == 0) ? a / b : Integer.MIN_VALUE);
+        operators.add((a, b) -> (a == 0 || a == 1 || b < 9) ? (int) Math.pow(a, b) : Integer.MIN_VALUE);
+        opnames.add('+');
+        opnames.add('-');
+        opnames.add('*');
+        opnames.add('/');
+    }
+
+    public void solusi(String[] num) {
+        List<Integer> digits = new ArrayList<Integer>();
+        digits.add(card.convertCardToInt(num[0]));
+        digits.add(card.convertCardToInt(num[1]));
+        digits.add(card.convertCardToInt(num[2]));
+        digits.add(card.convertCardToInt(num[3]));
+        ArrayList<String> solutions = new ArrayList<String>();
+        for (int a = 0; a < 4; a++) {
+            for (int b = 0; b < 4; b++) {
+                for (int c = 0; c < 4; c++) {
+                    for (int d = 0; d < 4; d++) {
+                        if ((a == b) || (a == c) || (a == d) || (b == c) || (b == d) || (c == d)) {
+                            continue;
+                        }
+                        for (int i = 0; i < 4; i++) {
+                            for (int j = 0; j < 4; j++) {
+                                for (int k = 0; k < 4; k++) {
+                                    if (operators.get(j).applyAsInt(operators.get(i).applyAsInt(digits.get(a), digits.get(b)), operators.get(k).applyAsInt(digits.get(c), digits.get(d))) == 24) {
+                                        String sol_str = "(%d %s %d) %s (%d %s %d)";
+                                        sol_str = String.format(sol_str, digits.get(a), opnames.get(i), digits.get(b), opnames.get(j), digits.get(c), opnames.get(k), digits.get(d));
+                                        solutions.add(sol_str);
+                                    }
+                                    if (operators.get(k).applyAsInt(operators.get(j).applyAsInt(operators.get(i).applyAsInt(digits.get(a), digits.get(b)), digits.get(c)), digits.get(d)) == 24) {
+                                        String sol_str = "((%d %s %d) %s %d) %s %d";
+                                        sol_str = String.format(sol_str, digits.get(a), opnames.get(i), digits.get(b), opnames.get(j), digits.get(c), opnames.get(k), digits.get(d));
+                                        solutions.add(sol_str);
+                                    }
+                                    if (operators.get(k).applyAsInt(operators.get(i).applyAsInt(digits.get(a), operators.get(j).applyAsInt(digits.get(b), digits.get(c))), digits.get(d)) == 24) {
+                                        String sol_str = "(%d %s (%d %s %d)) %s %d";
+                                        sol_str = String.format(sol_str, digits.get(a), opnames.get(i), digits.get(b), opnames.get(j), digits.get(c), opnames.get(k), digits.get(d));
+                                        solutions.add(sol_str);
+                                    }
+                                    if (operators.get(i).applyAsInt(digits.get(a), operators.get(k).applyAsInt(operators.get(j).applyAsInt(digits.get(b), digits.get(c)), digits.get(d))) == 24) {
+                                        String sol_str = "%d %s ((%d %s %d) %s %d)";
+                                        sol_str = String.format(sol_str, digits.get(a), opnames.get(i), digits.get(b), opnames.get(j), digits.get(c), opnames.get(k), digits.get(d));
+                                        solutions.add(sol_str);
+                                    }
+                                    if (operators.get(i).applyAsInt(digits.get(a), operators.get(j).applyAsInt(digits.get(b), operators.get(k).applyAsInt(digits.get(c), digits.get(d)))) == 24) {
+                                        String sol_str = "%d %s (%d %s (%d %s %d))";
+                                        sol_str = String.format(sol_str, digits.get(a), opnames.get(i), digits.get(b), opnames.get(j), digits.get(c), opnames.get(k), digits.get(d));
+                                        solutions.add(sol_str);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(solutions.size() + " Solutions found");
+        if(solutions.size()>0) {
+            for (String solution : solutions) {
+                System.out.println(solution);
+            }
+        } else {
+            System.out.println("Tidak ada solusi");
+        }
+    }
+}
+
+public class brute {
+}
--- src/main.java
+++ src/main.java
@@ -1,26 +1,63 @@
 import java.io.*;
 import java.util.*;
 
-import javax.xml.transform.Source;
-
 public class main{
+
+    public static List<String> randomList(){
+        List<String> arrayCard = new ArrayList<>();
+        arrayCard.add("A");arrayCard.add("2");arrayCard.add("3");arrayCard.add("4");
+        arrayCard.add("5");arrayCard.add("6");arrayCard.add("7");arrayCard.add("8");
+        arrayCard.add("9");arrayCard.add("10");arrayCard.add("J");arrayCard.add("Q");
+        arrayCard.add("K");
+        Collections.shuffle(arrayCard);
+        return arrayCard.subList(0, 4);
+    }
     public static void main(String[] args) throws IOException{
         Scanner input = new Scanner(System.in);
-        angka numerik = new angka();
-        int inputuser = 0;
+        Scanner masukan = new Scanner(System.in);
+        String kartu;
+
+        List<String> card_input = new ArrayList<String>();
+        boolean valid;
+        int inputuser = 0, n = 0;
         while(inputuser != 1 && inputuser != 2) {
             System.out.println("Pilih masukkan yang diinginkan: ");
             System.out.println("1. Random");
             System.out.println("2. Keyboard Sendiri");
             
+
             inputuser = input.nextInt();
             if (inputuser == 1){
-                numerik = new angka(1);
+                card_input = randomList();
             }
             else if (inputuser == 2){
-                numerik = new angka(2);
+            System.out.println("Masukkan 4 angka/huruf (A,2,3,4,5,6,7,8,9,10,J,Q,K)");
+            while (n < 4){
+                valid = false;
+                while (!valid){
+                    kartu = masukan.nextLine();
+                    if (card.convertCardToInt(kartu) == 0){
+                        System.out.println("Masukan Tidak Sesuai");
+                        }
+                    else{
+                        card_input.add(kartu);
+                        valid = true;
+                        n++;
+                        }
+                    }
+            
+                
+                }
             }
         }
-    input.close();
+        input.close();
+        String[] cards = (String[]) card_input.toArray(new String[0]);
+        DuaPuluhEmpat game = new DuaPuluhEmpat();
+        long startTime = System.currentTimeMillis();
+        game.solusi(cards);
+
+        long endTime = System.currentTimeMillis();
+        long totalTime = endTime - startTime;
+        System.out.println("Execution time: " + totalTime + " ms");
     }
 }
